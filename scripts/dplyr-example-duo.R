10409f8ae3a83bc606ef112664c0fe0eaabde4ee
Roel Hogervorst <hogervorst.rm@gmail.com>
2016-02-22 17:59:36 +0100

    Examplecode: dplyr-example-duo

---
+++ scripts/dplyr-example-duo.R
@@ -0,0 +1,4 @@
+#https://www.duo.nl/open_onderwijsdata/databestanden/ho/Ingeschreven/wo_ingeschr/Ingeschrevenen_wo1.jsp
+#
+link<-"https://www.duo.nl/open_onderwijsdata/images/01.%20Ingeschrevenen%20wo-2015.csv"
+library(readr)
